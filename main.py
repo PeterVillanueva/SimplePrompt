c0c36d12085261a56d54e97fffc3951e1b749061
Peter VIllanueva <peterdonniel4444@gmail.com>
2023-05-02 11:22:47 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+address = input("Enter Your Address: ")
+
+prompt1 = 'Your Name is '
+prompt2 = ' and your Age is '
+prompt3 = ' and you live at '
+
+print(prompt1 + name + prompt2 + age + prompt3 + address)
+
+
